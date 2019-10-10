9b317d97f90d40778b27947003f33fd1f5fee06e
Freyja <aerandil@gmail.com>
2019-10-11 00:14:25 +0200

    Preparing to use CI

--- tests/test_dice_roller.py
+++ tests/test_dice_roller.py
@@ -3,9 +3,9 @@ import unittest
 from utils.dice_roller import roll_die
 
 
-class TestMethods(unittest.TestCase):
+class TestDiceRoller(unittest.TestCase):
     def test_boundaries_roll_20(self):
         for i in range(100):
             a = roll_die(20)
-            self.assertTrue(a > 0)
-            self.assertTrue(a < 21)
+            assert(a > 0)
+            assert(a < 21)
