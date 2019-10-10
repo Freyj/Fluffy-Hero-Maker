50c30b4270833b0d26f77f3937d73ce114488115
Freyja <aerandil@gmail.com>
2019-10-11 00:20:33 +0200

    Correcting for CI

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='Fluffy-Hero-Maker',
+      version='0.5.0',
+      description='A software to help out tabletop role playing character creation and more',
+      author='Freyj',
+      author_email='',
+      url='https://github.com/Freyj/Fluffy-Hero-Maker',
+      packages=find_packages(),
+     )
